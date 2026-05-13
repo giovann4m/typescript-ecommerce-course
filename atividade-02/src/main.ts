ce5277ebec415e1e31b9c6d72722e25ccd612cbc
Giovanna <giovannamarques318@gmail.com>
2026-05-12 23:32:40 -0400

    chore: configura PrimeVue com tema Aura dark e ConfirmationService

--- atividade-02/src/main.ts
+++ atividade-02/src/main.ts
@@ -3,4 +3,22 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import ConfirmationService from 'primevue/confirmationservice'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.app',
+    }
+  }
+})
+
+app.use(ConfirmationService)
+
+app.mount('#app')
