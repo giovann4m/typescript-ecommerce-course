7e9a2059e0b0b3fe0a490f2f98e99e78bc797334
Giovanna <giovannamarques318@gmail.com>
2026-06-25 00:16:52 -0400

    feat: cria middleware de logger para registrar metodo e url das requisicoes

---
+++ atividade-06/src/logger.middleware.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from 'express'
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  const date = new Date().toISOString()
+  console.log(`[${date}] ${req.method} ${req.url}`)
+  next()
+}
